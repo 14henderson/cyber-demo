da2783d01436b0f63cbb5086a52ee90d4c487a52
UEA\xjh16zfu <xjh16zfu@uea.ac.uk>
2021-10-21 14:20:45 +0100

    initial push

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
+    
+    
+if __name__ == '__main__':
+	app.run(debug=True, host="0.0.0.0", port="8000")
